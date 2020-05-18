a7623893fac2cfbf5d737303eaf36462d9e5cb63
luke.sims <lukesims12@gmail.com>
2020-05-18 16:39:42 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Login from './components/Login';
+import Spotify from 'spotify-web-api-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const spotifyWebApi = new Spotify();
 
-export default App;
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        const params = this.getHashParams();
+        this.state = {
+            loggedIn: params.access_token ? true : false,
+            recievedResponse: false,
+            nowPlaying: {
+                name: "Not Checked",
+                image: ""
+            }
+        }
+        if (params.access_token) {
+            spotifyWebApi.setAccessToken(params.access_token);
+        } 
+    }
+kwme
+    getMusicImage = () => {
+        return (
+            <div>
+                <img 
+                    src={this.state.nowPlaying.image} 
+                    alt="now-playing-artist"
+                    style={{width: 400}}
+                />             
+            </div>
+        )
+    }
+
+    getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        while ((e = r.exec(q))) {
+           hashParams[e[1]] = decodeURIComponent(e[2]);
+        }
+        return hashParams;
+    }
+
+    getNowPlaying = () => {
+        spotifyWebApi.getMyCurrentPlaybackState()
+            .then((response) => {
+                if (response) {
+                    this.setState({
+                        recievedResponse: true,
+                        nowPlaying: {
+                            name: response.item.name,
+                            image: response.item.album.images[0].url
+                        }
+                    });
+                } else {
+                    this.setState({
+                        nowPlaying: {
+                            name: "Nothing currently playing!",
+                            image: ""
+                        }
+                    })
+                }
+            })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {!this.state.loggedIn ? <Login /> : '' }                 
+                <div>Now Playing : {(this.state.nowPlaying.name !== '') ? this.state.nowPlaying.name : "Nothing is playing"}</div>
+                {this.state.recievedResponse ? this.getMusicImage() : ''}
+                <button onClick={() => this.getNowPlaying()}>Check Now Playing</button>
+            </div>
+        );
+
+    }
+}
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <a href="http://localhost:8888/login" className="btn-spotify-login">                    
+                <i className="fab fa-spotify"></i> Login to Spotify
+            </a>
+        </div>
+    );
+}
+
+export default Login;
